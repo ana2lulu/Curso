748c8caa12e5a9bc3d64c972fd866acfe54e610a
Ana luiza <129326664+ana2lulu@users.noreply.github.com>
2024-10-08 12:00:17 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,183 @@
+import './App.css';
+import aula from "./aula.png";
+import aula2 from "./aula2.png";
+import logo from "./logo.png";
+import play from "./play.png";
+import  perfil from "./perfil.png"
+
+
+function App() {
+  return (
+
+    <div>
+      
+    <div className="App-header1">
+      <h1 className="h1"> Aulas </h1>
+      <header className="App-header">
+
+    
+      
+      </header>
+
+      <div className="input-section-header">
+        <div className="quadradinho redondo1">Animais</div>
+        <div className="quadradinho redondo2">Saudações</div>
+        <div className="quadradinho redondo3">Comidas</div>
+      </div>
+
+    
+      <div>
+      <img src={perfil} className="perfil" alt="perfil"></img>
+   </div>
+
+    
+      
+      <div>
+        <img src={aula} className="img1" alt="aula" />
+      </div>
+
+      <div>
+        <img src={aula} className="img2" alt="aula" />
+      </div>
+      
+
+      <div>
+        <img src={aula} className="img3" alt="aula" />
+      </div>
+
+      <div>
+        <img src={aula} className="img4" alt="aula" />
+      </div>
+
+      <div>
+        <img src={aula} className="img5" alt="aula" />
+      </div>
+      
+
+      <div>
+      <img src={aula2} className="aula2" alt="aula2"></img>
+    </div>
+
+    <div>
+      <img src={aula2} className="aula3" alt="aula3"></img>
+    </div>
+
+    
+    <div>
+      <img src={aula2} className="aula4" alt="aula4"></img>
+    </div>
+
+
+    <div>
+      <img src={aula2} className="aula5" alt="aula5"></img>
+    </div>
+
+    <div>
+      <img src={aula2} className="aula6" alt="aula6"></img>
+    </div>
+
+    <div>
+      <img src={aula2} className="aulaf" alt="aulaf"></img>
+    </div>
+
+    
+    
+    <div>
+      <img src={aula2} className="aulay" alt="aulay"></img>
+    </div>
+
+
+
+    <div>
+      <img src={aula2} className="aulas" alt="aulas"></img>
+    </div>
+
+    
+    <div>
+      <img src={aula2} className="aulak" alt="aulak"></img>
+    </div>
+
+    
+    <div>
+      <img src={aula2} className="aulal" alt="aulal"></img>
+    </div>
+
+
+    <div>
+        <img src={logo} className="logo" alt="logo"></img>
+      </div>
+
+
+<div>
+
+<div>
+        <img src={play} className="play" alt="play"></img>
+</div>
+
+<div>
+        <img src={play} className="play2" alt="play2"></img>
+ </div>
+
+ <div>
+        <img src={play} className="play3" alt="play3"></img>
+ </div>
+ <div>
+        <img src={play} className="play4" alt="play4"></img>
+
+</div>
+        <img src={play} className="play5" alt="play5"></img>
+
+      </div>
+
+    <div>
+
+        <img src={play} className="playA" alt="playA"></img>
+  
+    </div>
+
+    <div>
+
+    <img src={play} className="playB" alt="playB"></img>
+  
+    </div>
+
+    <div>
+
+  <img src={play} className="playC" alt="playC"></img>
+
+  </div>
+   <img src={play} className="playD" alt="playD"></img>
+   
+  <div>
+  <img src={play} className="playE" alt="playE"></img>
+  
+  </div>
+   
+  <img src={play} className="playR" alt="playR"></img>
+   
+<div>
+<img src={play} className="playT" alt="playT"></img>
+
+<div>
+<img src={play} className="playY" alt="playY"></img>
+</div>
+
+</div>
+<img src={play} className="playH" alt="playH"></img>
+
+ 
+<div>
+<img src={play} className="playK" alt="playK"></img>
+
+   </div>
+</div>
+
+   </div>
+
+  
+
+    
+  );
+}
+
+export default App;
